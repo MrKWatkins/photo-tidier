5dba9c59f7f821e5581e9c8d1253afa03a74290c
MrKWatkins <mrkwatkins@gmail.com>
2020-12-05 12:22:35 +0000

    Add an input directory to command line options.

---
+++ src/configuration.rs
@@ -0,0 +1,16 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "photo-tidier", about = "A simple application to tidy up photos.")]
+pub struct CommandLineArguments
+{
+    /// The source folder.
+    #[structopt(parse(from_os_str))]
+    pub source: PathBuf
+}
+
+pub fn get() -> CommandLineArguments
+{
+    return CommandLineArguments::from_args();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+mod configuration;
+
+use std::fs;
+use std::io;
+use std::path::Path;
+
+fn main()
+{
+    let arguments = configuration::get();
+
+    output_directory(arguments.source.as_path())
+        .unwrap_or_else(|e| println!("Unexpected error: {}", e.to_string()));
+}
+
+fn output_directory(directory: &Path) -> io::Result<()>
+{
+    println!("Directory: {}", directory.display().to_string());
+
+    for entry in fs::read_dir(directory)?
+    {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file()
+        {
+            if let Some(file_name) = path.file_name().and_then(|f| f.to_str())
+            {
+                println!("{}", file_name);
+            }
+        }
+    }
+    Ok(())
 }
